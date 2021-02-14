2ab8b8a0dadfa4d8fd7aed2dbe9589ad87821ed3
Misha <xxxdaddy420@gmail.com>
2021-02-14 19:15:02 +0700

    connected jquery for modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -19,20 +19,6 @@ $(document).ready(function(){
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');  
     });
-    // $('.catalog-item__link').each(function(i) {
-    //   $(this).on('click', function(event) {
-    //     event.preventDefault();
-    //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //   });
-    // });
-    // $('.catalog-item__back').each(function(i) {
-    //   $(this).on('click', function(event) {
-    //     event.preventDefault();
-    //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //   });
-    // });
     function toggleSlide(item) {
       $(item).each(function(i) {
         $(this).on('click', function(event) {
@@ -45,5 +31,19 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-
+    $('[data-modal="consultation"]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('fast');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
+    });
+    // $('.button_mini').on('click', function() {
+    //   $('.overlay, #order').fadeIn();
+    // });
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__subtitle').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('fast');
+      });
+    });
 });
