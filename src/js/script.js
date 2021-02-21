6283cb8e6a5f4a11af29b4722f75f4bc55b3674a
Misha <xxxdaddy420@gmail.com>
2021-02-21 13:25:37 +0700

    added validation forms

--- src/js/script.js
+++ src/js/script.js
@@ -31,19 +31,46 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+
+    //modal
     $('[data-modal="consultation"]').on('click', function() {
       $('.overlay, #consultation').fadeIn('fast');
     });
     $('.modal__close').on('click', function() {
       $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
     });
-    // $('.button_mini').on('click', function() {
-    //   $('.overlay, #order').fadeIn();
-    // });
     $('.button_mini').each(function(i) {
       $(this).on('click', function() {
         $('#order .modal__subtitle').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay, #order').fadeIn('fast');
       });
     });
+
+    
+    //validate 
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: "required",
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: "Пожалуйста, введите свое имя",
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+      });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 });
